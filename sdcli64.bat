05c572f2ad88ad6d079ad8fa0f2f1c07e35810ae
CarmelonHaldon <carmelonhaldon@gmail.com>
2019-06-04 17:36:18 +0200

    @CarmelonHaldon...

--- sdcli64.bat
+++ sdcli64.bat
@@ -1,5 +1,5 @@
 rmdir %1 /s /q
-rem TODO CarmelonHaldon: setlocal para que no se repita??
+rem TODO @CarmelonHaldon: setlocal para que no se repita??
 set PATH=%5;%PATH%
 %6\sdcli64.exe cart export -cart %2 -cfg %3 -log %4
 rem move %1\Generado*.sql %2.sql
